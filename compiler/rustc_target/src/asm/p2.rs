dd664ffe3fbdcedfc2d2193704cbd52c70f1f6b1
ne75 <ne75@cornell.edu>
2022-07-23 23:11:38 -0700

    updates

--- compiler/rustc_target/src/asm/p2.rs
+++ compiler/rustc_target/src/asm/p2.rs
@@ -35,7 +35,7 @@ impl P2InlineAsmRegClass {
         arch: InlineAsmArch,
     ) -> &'static [(InlineAsmType, Option<Symbol>)] {
         match (self, arch) {
-            (Self::reg, _) => types! { _: I8, I16; },
+            (Self::reg, _) => types! { _: I8, I16, I32; },
         }
     }
 }
